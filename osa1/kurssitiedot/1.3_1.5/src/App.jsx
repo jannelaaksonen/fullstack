88222cb3928a69971f9373d3d38868124d5a88ea
Janne Laaksonen <jannelaaksonen86@gmail.com>
2023-11-13 20:11:55 +0200

    1.3 - 1.5 aloitus

---
+++ osa1/kurssitiedot/1.3_1.5/src/App.jsx
@@ -0,0 +1,57 @@
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+  const Header = (props) => {
+    return (
+      <div>
+        <h1>{props.course}</h1>
+      </div>
+    )
+  }
+  const Content = (props) => {
+    console.log(props);
+    return (
+      <div>
+        <Part part={props.part1.name} exercises={props.part1.exercises}/>
+        <Part part={props.part2.name} exercises={props.part2.exercises}/>
+        <Part part={props.part3.name} exercises={props.part3.exercises}/>
+      </div>
+    )
+  }
+  const Part = (props) => {
+    return (
+        <p>{props.part} {props.exercises}</p>
+    )
+
+  }
+  const Total = (props) => {
+    return (
+      <div>
+        <p>Number of exercises {part1.exercises + part2.exercises + part3.exercises}</p>
+      </div>
+    )
+  }
+  return (
+    <div>
+      <Header course={course}/>
+      <Content 
+      part1={part1} 
+      part2={part2} 
+      part3={part3}/>
+      <Total exercises1={part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises}/>
+    </div>
+  )
+}
+
+export default App
