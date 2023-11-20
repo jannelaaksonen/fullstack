d9751b47843de8f9773dca06408664b84294c0d4
Janne Laaksonen <jannelaaksonen86@gmail.com>
2023-11-20 16:39:25 +0200

    1.7 valmis

--- osa1/unicafe/1.6_1.14/src/App.jsx
+++ osa1/unicafe/1.6_1.14/src/App.jsx
@@ -1,26 +1,74 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
+  const [totalPoints, setTotalPoints] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [positive, setPositive] = useState(0)
+
+  // useEffect hookki päivittää keskiarvon heti kun totalPoints tai total muuttuu
+  useEffect(() => {
+    const updatedAverage = totalPoints / total;
+    setAverage(updatedAverage);
+    console.log(updatedAverage);
+  }, [totalPoints, total]);
+  // sama kuin yllä, mutta positiivisen prosenttiosuuden laskemiseen
+  useEffect(() => {
+    const updatedPositive = good / total * 100;
+    setPositive(updatedPositive);
+    console.log(updatedPositive);
+  }
+
+  )
+
   
+  //good, neutral, bad counters
   const handleGoodClick = () => {
-    setGood(good + 1)
-    console.log('good', good)
+    const updatedGood = good + 1
+    setGood(updatedGood)
+    const updatedTotal = total + 1
+    setTotal(updatedTotal)
+    const updatedTotalPoints = totalPoints + 1
+    setTotalPoints(updatedTotalPoints)
+    const updatedAverage = totalPoints / total
+    setAverage(updatedAverage)
+    const updatedPositive = good / total * 100
+    setPositive(updatedPositive)
+    console.log(average)
   }
 
   const handleNeutralClick = () => {
-    setNeutral(neutral + 1)
-    console.log('neutral', neutral)
+    const updatedNeutral = neutral + 1
+    setNeutral(updatedNeutral)
+    const updatedTotal = total + 1
+    setTotal(updatedTotal)
+    const updatedTotalPoints = totalPoints + 0
+    setTotalPoints(updatedTotalPoints)
+    const updatedAverage = totalPoints / total
+    setAverage(updatedAverage)
+    const updatedPositive = good / total * 100
+    setPositive(updatedPositive)
+    console.log(average)
   }
 
   const handleBadClick = () => {
-    setBad(bad + 1)
-    console.log('bad', bad)
+    const updatedBad = bad + 1
+    setBad(updatedBad)
+    const updatedTotal = total + 1
+    setTotal(updatedTotal)
+    const updatedTotalPoints = totalPoints - 1
+    setTotalPoints(updatedTotalPoints)
+    const updatedAverage = totalPoints / total
+    setAverage(updatedAverage)
+    const updatedPositive = good / total * 100
+    setPositive(updatedPositive)
+    console.log(average)
   }
-
+  //renderöidään tiedot näytölle
   return (
     <div>
       <h1>give feedback</h1>
@@ -31,6 +79,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {total}</p>
+      <p>average {average}</p>
+      <p>positive {positive}</p>
     </div>
   )
 }
