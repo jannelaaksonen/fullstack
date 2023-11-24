6117c4f42784e80261792bf1ad5a3ecd20e03ae5
Janne Laaksonen <jannelaaksonen86@gmail.com>
2023-11-24 09:05:15 +0200

    1.5 valmis

--- osa1/kurssitiedot/1.3_1.5/src/App.jsx
+++ osa1/kurssitiedot/1.3_1.5/src/App.jsx
@@ -1,19 +1,53 @@
 const App = () => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+  const Header = (props) => {
+    console.log(props)
+    return <h1>{props.course}</h1>
+  }
+  const Content = (props) => {
+    console.log(props);
+    return (
+      <div>
+        <Part part={props.parts[0].name} exercises={props.parts[0].exercises}/>
+        <Part part={props.parts[1].name} exercises={props.parts[1].exercises}/>
+        <Part part={props.parts[2].name} exercises={props.parts[2].exercises}/>
+      </div>
+    )
+  }
+  const Part = (props) => {
+    return (
+        <p>{props.part} {props.exercises}</p>
+    )
 
+  }
+  const Total = (props) => {
+    return (
+      <div>
+        <p>Number of exercises {course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises}</p>
+      </div>
+    )
+  }
   return (
     <div>
-      <div>
-        {left}
-        <button onClick={() => setLeft(left + 1)}>
-          left
-        </button>
-        <button onClick={() => setRight(right + 1)}>
-          right
-        </button>
-        {right}
-      </div>
+      <Header course={course.name}/>
+      <Content parts={course.parts}/>
+      <Total exercises1={course.parts[0].exercises} exercises2={course.parts[1].exercises} exercises3={course.parts[2].exercises}/>
     </div>
   )
 }
