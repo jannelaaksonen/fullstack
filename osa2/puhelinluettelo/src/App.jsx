4ae86a1c2a0a03900d753f758addc10ccbd18a37
jannelaaksonen <jannelaaksonen86@gmail.com>
2023-12-14 10:36:28 +0200

    2.9 valmis

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -2,10 +2,14 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
   const [newName, setNewName] = useState('')
   const [newPhoneNumber, setNewPhonenumber] = useState('')
+  const [newFilter, setFilter] = useState('')
 
   // uuden henkilön lisäys ja tarkistus ettei nimi ole jo luettelossa
   const addNewPerson = (event) => {
@@ -22,7 +26,7 @@ const App = () => {
       setNewPhonenumber('')
     }
   }; 
-  
+
   
   // uuden henkilön nimen käsittely
   const handleNewPerson = (event) => {
@@ -35,11 +39,27 @@ const App = () => {
     console.log(event.target.value)
     setNewPhonenumber(event.target.value)
   }
+  // filtterin käsittely
+  const addNewFilter = (event) => {
+    console.log(event.target.value)
+    setFilter(event.target.value)
+  }
+  // suodatetaan esitettävistä nimistä ne, jotka sisältävät filtterin
+  const filteredPersons = persons.filter(person => {
+    const nameMatches = person.name.toLowerCase().includes(newFilter.toLowerCase());
+    const numberMatches = person.number.toLowerCase().includes(newFilter.toLowerCase());
+    return nameMatches || numberMatches;
+  });
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>Filter shown with <input
+            value={newFilter}
+            onChange={addNewFilter}/>
+      </div>
       <div>
+        <h2>Add a new</h2>
         <form onSubmit={addNewPerson}>
             <div>Name: <input 
             value={newName}
@@ -48,14 +68,15 @@ const App = () => {
             <div>Number: <input 
             value={newPhoneNumber}
             onChange={handleNewPhoneNumber}
-            /></div>
+            />
+            </div>
             <div><button type="submit">add</button></div>
         </form>
       </div>
       <h2>Numbers</h2>
       <ul>
-        {persons.map(person =>
-          <li key={person.name}>{person.name}</li>
+        {filteredPersons.map(person =>
+          <li key={person.name}>{person.name} {person.number}</li>
         )}
       </ul>
     </div>
